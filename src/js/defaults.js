185894114c10f9581c3c99edd5e4a23d47c5c578
Amreet Khuntia <amreetleo123@gmail.com>
2024-10-11 01:09:44 +0530

    feat: added log support for date and minor formats
    - added support for date
    - added support for formats
    - added support for separators and spacings
    - changed color formats

--- src/js/defaults.js
+++ src/js/defaults.js
@@ -4,12 +4,23 @@
  */
 
 export const defaultDisplaySettings = [
-    'ORDERID',       // Display order ID
-    'SESSIONID',     // Display session ID
-    'PROCESSID',     // Display process ID
-    'FUNCTIONNAME',  // Display function name
-    'TYPE',          // Display type
-    'BODY'           // Display body
+  "TIME", // Display time
+  "ORDERID", // Display order ID
+  "SESSIONID", // Display session ID
+  "PROCESSID", // Display process ID
+  "FUNCTIONNAME", // Display function name
+  "TYPE", // Display type
+  "BODY", // Display body
+];
+
+export const availableKeys = [
+  "TIME", // Display time
+  "ORDERID", // Display order ID
+  "SESSIONID", // Display session ID
+  "PROCESSID", // Display process ID
+  "FUNCTIONNAME", // Display function name
+  "TYPE", // Display type
+  "BODY", // Display body
 ];
 
 /**
@@ -18,12 +29,13 @@ export const defaultDisplaySettings = [
  */
 
 export const keyToSettingMap = {
-    ORDERID: 'orderId',             // Map ORDERID setting to orderId object key
-    SESSIONID: 'sessionId',         // Map SESSIONID setting to sessionId object key
-    PROCESSID: 'processId',         // Map PROCESSID setting to processId object key
-    FUNCTIONNAME: 'functionName',   // Map FUNCTIONNAME setting to functionName object key
-    TYPE: 'type',                   // Map TYPE setting to type object key
-    BODY: 'body'                    // Map BODY setting to body object key
+  ORDERID: "orderId", // Map ORDERID setting to orderId object key
+  SESSIONID: "sessionId", // Map SESSIONID setting to sessionId object key
+  PROCESSID: "processId", // Map PROCESSID setting to processId object key
+  FUNCTIONNAME: "functionName", // Map FUNCTIONNAME setting to functionName object key
+  TYPE: "type", // Map TYPE setting to type object key
+  BODY: "body", // Map BODY setting to body object key
+  TIME: "time", // Show Currenct Time
 };
 
 /**
@@ -31,49 +43,49 @@ export const keyToSettingMap = {
  */
 
 export const colorsMap = {
-    reset: "\x1b[0m",
-    bright: "\x1b[1m",
-    dim: "\x1b[2m",
-    underscore: "\x1b[4m",
-    blink: "\x1b[5m",
-    reverse: "\x1b[7m",
-    hidden: "\x1b[8m",
-    
-    // Foreground colors
-    fgBlack: "\x1b[30m",
-    fgRed: "\x1b[31m",
-    fgGreen: "\x1b[32m",
-    fgYellow: "\x1b[33m",
-    fgBlue: "\x1b[34m",
-    fgMagenta: "\x1b[35m",
-    fgCyan: "\x1b[36m",
-    fgWhite: "\x1b[37m",
-    fgGray: "\x1b[90m",
-    fgBrightRed: "\x1b[91m",
-    fgBrightGreen: "\x1b[92m",
-    fgBrightYellow: "\x1b[93m",
-    fgBrightBlue: "\x1b[94m",
-    fgBrightMagenta: "\x1b[95m",
-    fgBrightCyan: "\x1b[96m",
-    fgBrightWhite: "\x1b[97m",
-    
-    // Background colors
-    bgBlack: "\x1b[40m",
-    bgRed: "\x1b[41m",
-    bgGreen: "\x1b[42m",
-    bgYellow: "\x1b[43m",
-    bgBlue: "\x1b[44m",
-    bgMagenta: "\x1b[45m",
-    bgCyan: "\x1b[46m",
-    bgWhite: "\x1b[47m",
-    bgGray: "\x1b[100m",
-    bgBrightRed: "\x1b[101m",
-    bgBrightGreen: "\x1b[102m",
-    bgBrightYellow: "\x1b[103m",
-    bgBrightBlue: "\x1b[104m",
-    bgBrightMagenta: "\x1b[105m",
-    bgBrightCyan: "\x1b[106m",
-    bgBrightWhite: "\x1b[107m"
+  reset: "\x1b[0m",
+  bright: "\x1b[1m",
+  dim: "\x1b[2m",
+  underscore: "\x1b[4m",
+  blink: "\x1b[5m",
+  reverse: "\x1b[7m",
+  hidden: "\x1b[8m",
+
+  // Foreground colors
+  fgBlack: "\x1b[30m",
+  fgRed: "\x1b[31m",
+  fgGreen: "\x1b[32m",
+  fgYellow: "\x1b[33m",
+  fgBlue: "\x1b[34m",
+  fgMagenta: "\x1b[35m",
+  fgCyan: "\x1b[36m",
+  fgWhite: "\x1b[37m",
+  fgGray: "\x1b[90m",
+  fgBrightRed: "\x1b[91m",
+  fgBrightGreen: "\x1b[92m",
+  fgBrightYellow: "\x1b[93m",
+  fgBrightBlue: "\x1b[94m",
+  fgBrightMagenta: "\x1b[95m",
+  fgBrightCyan: "\x1b[96m",
+  fgBrightWhite: "\x1b[97m",
+
+  // Background colors
+  bgBlack: "\x1b[40m",
+  bgRed: "\x1b[41m",
+  bgGreen: "\x1b[42m",
+  bgYellow: "\x1b[43m",
+  bgBlue: "\x1b[44m",
+  bgMagenta: "\x1b[45m",
+  bgCyan: "\x1b[46m",
+  bgWhite: "\x1b[47m",
+  bgGray: "\x1b[100m",
+  bgBrightRed: "\x1b[101m",
+  bgBrightGreen: "\x1b[102m",
+  bgBrightYellow: "\x1b[103m",
+  bgBrightBlue: "\x1b[104m",
+  bgBrightMagenta: "\x1b[105m",
+  bgBrightCyan: "\x1b[106m",
+  bgBrightWhite: "\x1b[107m",
 };
 
 /**
@@ -82,28 +94,59 @@ export const colorsMap = {
  */
 
 export const defaultKeyColorMap = {
-    orderId: {
-        fgColor: colorsMap.fgBlack,
-        bgColor: colorsMap.bgBrightMagenta,
-    },
-    sessionId: {
-        fgColor: colorsMap.fgBlack,
-        bgColor: colorsMap.bgCyan,
-    },
-    processId: {
-        fgColor: colorsMap.fgBlack,
-        bgColor: colorsMap.bgBrightGreen,
-    },
-    functionName: {
-        fgColor: colorsMap.fgBlack,
-        bgColor: colorsMap.bgBrightRed,
-    },
-    type: {
-        fgColor: colorsMap.fgBlack,
-        bgColor: colorsMap.bgGreen,
-    },
-    body: {
-        fgColor: "",
-        bgColor: "",
-    }
-}
+  orderId: {
+    fgColor: colorsMap.fgBrightMagenta,
+    bgColor: "",
+  },
+  sessionId: {
+    fgColor: colorsMap.fgBrightWhite,
+    bgColor: "",
+  },
+  processId: {
+    fgColor: colorsMap.fgBrightRed,
+    bgColor: "",
+  },
+  functionName: {
+    fgColor: colorsMap.fgBrightCyan,
+    bgColor: "",
+  },
+  type: {
+    fgColor: colorsMap.bright + colorsMap.fgBrightYellow,
+    bgColor: "",
+  },
+  body: {
+    fgColor: "",
+    bgColor: "",
+  },
+  time: {
+    fgColor: colorsMap.fgBrightGreen,
+    bgColor: "",
+  },
+};
+
+export const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export const defaultSeparator = ",";
--- src/js/index.js
+++ src/js/index.js
@@ -1,61 +1,102 @@
-import { defaultDisplaySettings, keyToSettingMap, colorsMap, defaultKeyColorMap } from './defaults.js';
+import {
+  defaultDisplaySettings,
+  keyToSettingMap,
+  colorsMap,
+  defaultKeyColorMap,
+  months,
+  days,
+  defaultSeparator,
+} from "./defaults.js";
 
-/** 
+/**
  * * ProcessLogger class for logging process information.
  * & Allows customization of the display order of process information fields.
  */
 class ProcessLogger {
-    // Default settings with displayOrder initialized to defaultDisplaySettings
-    settings = {
-        displayOrder: defaultDisplaySettings,
-        colorsMap: defaultKeyColorMap
-    }
+  // Default settings with displayOrder initialized to defaultDisplaySettings
+  settings = {
+    displayOrder: defaultDisplaySettings,
+    colorsMap: defaultKeyColorMap,
+    enablePrintSeparator: false,
+    printSeparator: defaultSeparator,
+    enablePrintSpaceBetweenLogKeys: true,
+  };
 
+  /**
+   * * Constructor for ProcessLogger class.
+   * @param {Object} settings - Settings for the logger.
+   */
+
+  constructor(settings) {
     /**
-     * * Constructor for ProcessLogger class.
-     * @param {Object} settings - Settings for the logger.
+     * & If settings are provided, override default settings
+     * TODO: Make more customisable
      */
 
-    constructor(settings,colorsMap) {
-        /**
-         * & If settings are provided, override default settings
-         * TODO: Make more customisable
-         */
-        
-        if (settings) this.settings = settings;
-        if(colorsMap) this.colorsMap = colorsMap
+    for (const key in settings) {
+      if (this.settings.hasOwnProperty(key)) {
+        this.settings[key] = settings[key];
+      }
+    }
+  }
+
+  /**
+   * * Function description for log
+   * & Logs process information based on the provided object.
+   * & Fields are logged in the order specified by the displayOrder setting.
+   * @param {Object} obj - Object containing process information.
+   * TODO: Add other logs like function called etc...
+   */
+
+  log(obj) {
+    let printString = ""; // Initialize printString to store the final string to be printed
+    const size = this.settings.displayOrder.length; // Size of the displayOrder array
+
+    // Iterate through each setting in the displayOrder
+    for (let i = 0; i < size; i++) {
+      const setting = this.settings.displayOrder[i];
+      let val;
+      const key = keyToSettingMap[setting];
+      if (setting !== "TIME") {
+        val = obj[key] ?? "null";
+      } else {
+        const now = new Date();
+        const currentTime = now.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        });
+
+        const day = days[now.getDay()];
+        const date = now.getDate(); // Get the day of the month (numeric date)
+        const month = months[now.getMonth()];
+        const year = now.getFullYear();
+
+        // Combine time, day of the week, numeric date, month, and year
+        const currentDate = `${day} : ${date}/${month}/${year}`;
+        val = "[" + currentTime + " : " + currentDate + "]";
+      }
+
+      printString += this.settings.colorsMap[key].fgColor;
+      printString += this.settings.colorsMap[key].bgColor;
+      if (this.settings.enablePrintSpaceBetweenLogKeys === true && i != 0) {
+        printString += " " + val;
+      } else {
+        printString += val;
+      }
+      printString += colorsMap.reset;
+      if (
+        i < size - 1 &&
+        this.settings.enablePrintSeparator === true &&
+        i != 0
+      ) {
+        printString += this.settings.printSeparator;
+      }
     }
 
-    /**
-     * * Function description for log
-     * & Logs process information based on the provided object.
-     * & Fields are logged in the order specified by the displayOrder setting.
-     * @param {Object} obj - Object containing process information.
-     * TODO: Add other logs like function called etc...
-     */
-
-    log(obj) {
-        let printString = ''; // Initialize printString to store the final string to be printed
-        const size = this.settings.displayOrder.length; // Size of the displayOrder array
-
-        // Iterate through each setting in the displayOrder
-        for (let i = 0; i < size; i++) {
-            const setting = this.settings.displayOrder[i];
-            const key = keyToSettingMap[setting]
-            const val = obj[key]; // Get the value corresponding to the current setting from the object
-
-            // Append the value to the printString
-            printString += this.settings.colorsMap[key].fgColor;
-            printString += this.settings.colorsMap[key].bgColor;
-            printString += " ";
-            printString += val;
-            printString += " "
-            printString += colorsMap.reset;
-        }
-
-        // Print the final string
-        console.log(printString);
-    }
+    // Print the final string
+    console.log(printString);
+  }
 }
 
 export default ProcessLogger;
