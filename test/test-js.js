7cdc7432886d3d6d852e879a9e339b9a82b6eafe
Amreet Khuntia <amreetleo123@gmail.com>
2024-10-11 01:09:44 +0530

    feat: added log support for date and minor formats
    - added support for date
    - added support for formats
    - added support for separators and spacings
    - changed color formats

--- test/test-js.js
+++ test/test-js.js
@@ -2,39 +2,55 @@
 import ProcessLogger from "../src/js/index.js";
 
 // Create a new instance of ProcessLogger
-const logger = new ProcessLogger();
+const logger = new ProcessLogger({
+  displayOrder: [
+    "TIME", // Display time
+    "PROCESSID", // Display process ID
+    "FUNCTIONNAME", // Display function name
+    "TYPE", // Display type
+    "BODY", // Display body
+  ],
+});
+
+function generateUUID() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0; // Generate a random number between 0 and 15
+    const v = c === "x" ? r : (r & 0x3) | 0x8; // If 'y', ensure the format
+    return v.toString(16); // Convert to hexadecimal
+  });
+}
 
 // Function to generate a random log object
 function generateRandomLog() {
-    // Generating a random orderId
-    const orderId = Math.floor(Math.random() * 1000).toString();
-    // Generating a random sessionId
-    const sessionId = Math.random().toString(36).substring(7);
-    // Generating a random processId
-    const processId = Math.random().toString(36).substring(7);
-    // Fixed value for functionName
-    const functionName = "function";
-    // Fixed value for type
-    const type = "types";
-    // Fixed value for body, represented as a JSON string
-    const body = JSON.stringify({
-        "orderId": "1",
-        "sessionId": "abcdef123456",
-        "processId": "obj",
-        "functionName": "function",
-        "type": "types",
-        "body": "bodies"
-    });
+  // Generating a random orderId
+  const orderId = Math.floor(Math.random() * 1000).toString();
+  // Generating a random sessionId
+  const sessionId = generateUUID();
+  // Generating a random processId
+  const processId = generateUUID();
+  // Fixed value for functionName
+  const functionName = "testingString";
+  // Fixed value for type
+  const type = "functionCalled";
+  // Fixed value for body, represented as a JSON string
+  const body = JSON.stringify({
+    orderId: "1",
+    sessionId: "abcdef123456",
+    processId: "obj",
+    functionName: "function",
+    type: "types",
+    body: "bodies",
+  });
 
-    // Return the generated log object
-    return {
-        orderId,
-        sessionId,
-        processId,
-        functionName,
-        type,
-        body
-    };
+  // Return the generated log object
+  return {
+    orderId,
+    sessionId,
+    processId,
+    functionName,
+    type,
+    body,
+  };
 }
 
 // Generate a random log object
