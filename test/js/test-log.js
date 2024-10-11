198ac05ecbac9a61394bdf5fbba6a17883fbe225
Amreet Khuntia <amreetleo123@gmail.com>
2024-10-11 23:59:12 +0530

    feat: changed defaults
    - changed default colors

--- test/js/test-log.js
+++ test/js/test-log.js
@@ -2,31 +2,10 @@
 import ProcessLogger from "../../src/js/index.js";
 
 // Create a new instance of ProcessLogger
-const logger = new ProcessLogger({
-  displayOrder: [
-    "TIME", // Display time,
-    "ORDERID",
-    "PROCESSID", // Display process ID
-    "FUNCTIONNAME", // Display function name
-    "TYPE", // Display type
-    "BODY", // Display body
-  ],
-});
-
-function generateUUID() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0; // Generate a random number between 0 and 15
-    const v = c === "x" ? r : (r & 0x3) | 0x8; // If 'y', ensure the format
-    return v.toString(16); // Convert to hexadecimal
-  });
-}
+const logger = new ProcessLogger();
 
 // Function to generate a random log object
 function testRandomLog() {
-  const sessionId = generateUUID();
-  // Generating a random processId
-  const processId = generateUUID();
-  // Fixed value for functionName
   const functionName = "testingString";
 
   const functionType = "functionCalled";
